d672df06ca5a80a9f8f345503eb7d018a42b6f55
J█████ <dev.nullinside@gmail.com>
2024-02-14 22:11:19 -0500

    Updating ports to prevent overlap

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
 docker build -t nullinside-api:latest .
 docker container stop nullinside-api
 docker container prune -f
-docker run -d --name=nullinside-api -p 8080:8080 -p 8081:8081 --restart unless-stopped nullinside-api:latest
+docker run -d --name=nullinside-api -p 8081:8080 -p 8082:8081 --restart unless-stopped nullinside-api:latest
