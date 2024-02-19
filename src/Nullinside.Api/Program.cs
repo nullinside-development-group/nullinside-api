97b20e53d09df0c55e1c58120363c877fa28ea98
J█████ <dev.nullinside@gmail.com>
2024-02-18 20:46:37 -0500

    wip

--- src/Nullinside.Api/Program.cs
+++ src/Nullinside.Api/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -10,13 +11,11 @@ var app = builder.Build();
 app.UsePathBase("/api/v1");
 
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
