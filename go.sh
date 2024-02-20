1a04befb3088b41abd2ba3ac7a03bba1d0d7932b
J█████ <dev.nullinside@gmail.com>
2024-02-19 21:28:32 -0500

    wip

--- go.sh
+++ go.sh
@@ -1,8 +1,4 @@
 docker build -t nullinside-api:latest .
 docker container stop nullinside-api
 docker container prune -f
-
-echo $MYSQL_SERVER
-echo $MYSQL_USERNAME
-echo $MYSQL_PASSWORD
 docker run -d --name=nullinside-api -e MYSQL_SERVER=$MYSQL_SERVER -e MYSQL_USERNAME=$MYSQL_USERNAME -e MYSQL_PASSWORD=$MYSQL_PASSWORD -p 8081:8080 -p 8082:8081 --restart unless-stopped nullinside-api:latest
