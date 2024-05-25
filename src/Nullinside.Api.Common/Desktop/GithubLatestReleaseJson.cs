1aa2f20ea7b965eb80be94c639fd1d514a4c2398
J█████ <dev.nullinside@gmail.com>
2024-05-25 15:14:56 -0400

    Adding GitHub helpers for checking version number

---
+++ src/Nullinside.Api.Common/Desktop/GithubLatestReleaseJson.cs
@@ -0,0 +1,16 @@
+﻿namespace Nullinside.Api.Common.Desktop;
+
+/// <summary>
+///   The response information from GitHub's API.
+/// </summary>
+public class GithubLatestReleaseJson {
+  /// <summary>
+  ///   The url of the resource.
+  /// </summary>
+  public string? html_url { get; set; }
+
+  /// <summary>
+  ///   The name of the release.
+  /// </summary>
+  public string? name { get; set; }
+}
