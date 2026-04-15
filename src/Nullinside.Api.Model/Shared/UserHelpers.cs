f83db8a0c6b3249520445b2eeb62109ee68a0f3a
J█████ <dev.nullinside@gmail.com>
2026-04-15 15:04:24 -0400

    feat: fixing bug in logging in not recording email
    closes nullinside-development-group/nullinside-api-twitch-bot#139

--- src/Nullinside.Api.Model/Shared/UserHelpers.cs
+++ src/Nullinside.Api.Model/Shared/UserHelpers.cs
@@ -64,6 +64,7 @@ public static class UserHelpers {
         });
       }
       else {
+        existing.Email = email;
         existing.Token = bearerToken;
         existing.RefreshToken = bearerRefreshToken;
         existing.TokenExpires = DateTime.UtcNow + tokenExpires;
