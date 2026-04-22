f4b1dbe2ba62a352915515b76fc61c6cf683bd48
J█████ <dev.nullinside@gmail.com>
2026-04-22 17:03:58 -0400

    chore: debugging

--- src/Nullinside.Api/Controllers/ContactUsController.cs
+++ src/Nullinside.Api/Controllers/ContactUsController.cs
@@ -247,11 +247,6 @@ public class ContactUsController : ControllerBase {
   }
 
   private void SendAdminEmail(string recipient, string subject, string product, string content, int feedbackId) {
-    LOG.Info($"EMAIL_HOST: {EMAIL_HOST}");
-    LOG.Info($"EMAIL_USERNAME: {EMAIL_USERNAME}");
-    LOG.Info($"EMAIL_PASSWORD: {EMAIL_PASSWORD}");
-    LOG.Info($"EMAIL_PORT: {EMAIL_PORT}");
-
     if (null == EMAIL_HOST || null == EMAIL_USERNAME || null == EMAIL_PASSWORD || null == EMAIL_PORT || !int.TryParse(EMAIL_PORT, out int port)) {
       return;
     }
@@ -325,10 +320,10 @@ public class ContactUsController : ControllerBase {
 
     User? otherPerson = null;
     if (feedback.UserId != userId) {
-      otherPerson = _dbContext.Users.FirstOrDefault(u => u.Id == Constants.ADMIN_USER_ID);
+      otherPerson = _dbContext.Users.FirstOrDefault(u => u.Id == feedback.UserId);
     }
     else {
-      otherPerson = _dbContext.Users.FirstOrDefault(u => u.Id == feedback.UserId);
+      otherPerson = _dbContext.Users.FirstOrDefault(u => u.Id == Constants.ADMIN_USER_ID);
     }
 
     if (null == otherPerson || string.IsNullOrWhiteSpace(otherPerson.Email)) {
